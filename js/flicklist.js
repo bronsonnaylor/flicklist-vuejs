78ed8bfb8a643cb4bdf7957fab01f01172ca9bc5
Bronson Naylor <bronsonnaylor@gmail.com>
2019-09-15 18:38:57 -0600

    Flicklist can now import movies and related data using an api. There is a known issue with add to watchlist buttons becoming active again if the search query is re-entered.

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,55 +1,47 @@
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "f6bd21e39046f6a08df40d82ad8cffd3"
+  token: "626aaeb749bede2aeb605c55c9025e93"
 }
 
 var flicklistView = new Vue({
 	el: '#mount-target',
 	data: function() {
 		return {
-			// This is the data model.
-			// Whenever it changes, Vue will automatically re-render
-			// the html for us.
 			watchlistItems: [],
-      browseItems: [],
+			browseItems: [],
+			searchTerm: []
       // TODO 8B
 		};
 	},
+
 	methods: {
 		discoverMovies: function () {
-			/**
-			 * Makes an AJAX request to themoviedb.org, asking for some movies
-			 * if successful, updates the data.browseItems appropriately
-			 */
-
 			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
-					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
-					.then((response) => {
-						console.log("We got a response from The Movie DB!");
-						console.log(response);
+			.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+			.then((response) => {
+				console.log("We got a response from The Movie DB!");
+				console.log(response);
 
-						this.browseItems = response.results;
+				this.browseItems = response.results;
 
-					});
-    },
-    searchMovies: function(searchTerm) {
-      // Make an AJAX request to the /search/movie endpoint
-      // of the API, using the query string that was passed in.
-      //
-      // if successful, update this.browseItems appropriately.
-      // This update will automatically trigger a re-render.
-      console.log(`searching for movies with "${searchTerm}" in their title...`);
+			});
+		},
+		searchMovies: function(searchTerm) {
+		console.log(`searching for movies with "${searchTerm}" in their title...`);
+		fetch(`${api.root}/search/movie?api_key=${api.token}&query=${this.searchTerm}`)
+		.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+		.then((response) => {
+			this.browseItems = response.results;
+		})
+		},
 
-      // TODO 9
-      // implement this function as described in the comment above
-      // you can use the body of discoverMovies as a jumping off point
-    },
 		addToWatchlist: function(movie) {
 			this.watchlistItems.push(movie);
 		},
 	},
+
 	mounted: function () {
 		this.discoverMovies();
-	},
+	}
 });
