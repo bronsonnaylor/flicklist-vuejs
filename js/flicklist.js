9a2f0f225efa0232e676be304d08756ebb6c91d1
Bronson Naylor <bronsonnaylor@gmail.com>
2019-09-22 21:16:14 -0600

    Completed flicklist studio 3

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,7 +1,7 @@
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO 0"
+  token: "626aaeb749bede2aeb605c55c9025e93"
 }
 
 var flicklistView = new Vue({
@@ -22,46 +22,47 @@ var flicklistView = new Vue({
 			 * Makes an AJAX request to themoviedb.org, asking for some movies
 			 * if successful, updates the data.browseItems appropriately
 			 */
-
 			fetch(`${api.root}/discover/movie?api_key=${api.token}`)
-					.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
-					.then((response) => {
-						console.log("We got a response from The Movie DB!");
-						console.log(response);
+			.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+			.then((response) => {
+				console.log("We got a response from The Movie DB!");
+				console.log(response);
+				this.browseItems = response.results;
+			});
+		},
+		searchMovies: function(searchTerm) {
+			// Make an AJAX request to the /search/movie endpoint
+			// of the API, using the query string that was passed in.
+			//
+			// if successful, update this.browseItems appropriately.
+			// This update will automatically trigger a re-render.
+			console.log(`searching for movies with "${searchTerm}" in their title...`);
 
-						this.browseItems = response.results;
+			fetch(`${api.root}/search/movie?api_key=${api.token}&query=${searchTerm}`)
+			.then(resp => resp.ok ? resp.json() : Promise.reject(resp))
+			.then((response) => {
+				console.log("We got a response from The Movie DB!");
+				console.log(response);
 
-					});
-    },
-    searchMovies: function(searchTerm) {
-      // Make an AJAX request to the /search/movie endpoint
-      // of the API, using the query string that was passed in.
-      //
-      // if successful, update this.browseItems appropriately.
-      // This update will automatically trigger a re-render.
-      console.log(`searching for movies with "${searchTerm}" in their title...`);
+				this.browseItems = response.results;
 
-      fetch(`${api.root}/search/movie?api_key=${api.token}&query=${searchTerm}`)
-      .then(resp => resp.ok ? resp.json() : Promise.reject(resp))
-      .then((response) => {
-        console.log("We got a response from The Movie DB!");
-        console.log(response);
+			});
+		},
 
-        this.browseItems = response.results;
+		posterUrl: function(movie) {
+			return "http://image.tmdb.org/t/p/w300//" + movie.poster_path;
+		},
 
-      });
-
-    },
-    posterUrl: function(movie) {
-      // TODO 4b
-      // implement this function
-
-      return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg"
-    },
 		addToWatchlist: function(movie) {
 			this.watchlistItems.push(movie);
+			console.log(this.watchlistItems)
 		},
+
+		removeFromWatchlist: function(movie) {
+			this.watchlistItems = this.watchlistItems.filter(newList => newList !== movie);
+		}
 	},
+
 	mounted: function () {
 		this.discoverMovies();
 	},
